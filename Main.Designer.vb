415b210208075db501242921ac935ecc15ca2df8
Anand Vijay Rajsri <avrajsri@gmail.com>
2019-03-22 15:57:17 +0530

    Add files via upload

---
+++ Main.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Box1 = New System.Windows.Forms.TextBox()
+        Me.Box3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Box2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Number"
+        '
+        'Box1
+        '
+        Me.Box1.Location = New System.Drawing.Point(123, 31)
+        Me.Box1.Name = "Box1"
+        Me.Box1.Size = New System.Drawing.Size(113, 20)
+        Me.Box1.TabIndex = 1
+        Me.Box1.Text = "0"
+        Me.Box1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Box3
+        '
+        Me.Box3.BackColor = System.Drawing.Color.White
+        Me.Box3.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.Box3.Location = New System.Drawing.Point(123, 126)
+        Me.Box3.Name = "Box3"
+        Me.Box3.ReadOnly = True
+        Me.Box3.Size = New System.Drawing.Size(113, 20)
+        Me.Box3.TabIndex = 2
+        Me.Box3.Text = "Show User Input"
+        Me.Box3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(22, 177)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 47)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Run"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(156, 177)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 47)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "User Input"
+        '
+        'Box2
+        '
+        Me.Box2.Enabled = False
+        Me.Box2.Location = New System.Drawing.Point(123, 80)
+        Me.Box2.Name = "Box2"
+        Me.Box2.Size = New System.Drawing.Size(113, 20)
+        Me.Box2.TabIndex = 6
+        Me.Box2.Text = "Null"
+        Me.Box2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Answer"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(217, 242)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(56, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "By. Anand"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(275, 259)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Box2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Box3)
+        Me.Controls.Add(Me.Box1)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Number 2 Word"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Box1 As TextBox
+    Friend WithEvents Box3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Box2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ Main.vb
@@ -0,0 +1,120 @@
+﻿Public Class Main
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Box2.Text = "Null"
+        Try
+            Box3.Text = Int(Box1.Text)
+            If (0 > Int(Box1.Text)) Then
+                Box3.Text = "Only +Ve Number"
+
+            ElseIf (100 < Int(Box1.Text)) Then
+                Box3.Text = "Only Number(0-100)"
+
+            Else
+                Box2.Text = Int(Box1.Text)
+                Box3.Text = "Wait..."
+                Dim x As String
+                Dim l As Integer
+                x = Box2.Text
+                l = Len(x)
+
+                If (l = 1) Then
+                    Select Case Int(Box1.Text)
+                        Case "0" : Box3.Text = "ZERO"
+                        Case "1" : Box3.Text = "ONE"
+                        Case "2" : Box3.Text = "TWO"
+                        Case "3" : Box3.Text = "THREE"
+                        Case "4" : Box3.Text = "FOUR"
+                        Case "5" : Box3.Text = "FIVE"
+                        Case "6" : Box3.Text = "SIX"
+                        Case "7" : Box3.Text = "SEVEN"
+                        Case "8" : Box3.Text = "EIGHT"
+                        Case "9" : Box3.Text = "NINE"
+                    End Select
+
+                ElseIf (l = 3) Then
+                    Box3.Text = "ONE HUNDRED"
+
+                Else
+                    If (Int(Box1.Text) < "21" Or Int(Box1.Text) = "30" Or Int(Box1.Text) = "40" Or Int(Box1.Text) = "50" Or Int(Box1.Text) = "60" Or Int(Box1.Text) = "70" Or Int(Box1.Text) = "80" Or Int(Box1.Text) = "90") Then
+
+                        Select Case Int(Box1.Text)
+                            Case "10" : Box3.Text = "TEN"
+                            Case "11" : Box3.Text = "ELEVEN"
+                            Case "12" : Box3.Text = "TWELVE"
+                            Case "13" : Box3.Text = "THIRTEEN"
+                            Case "14" : Box3.Text = "FOURTEEN"
+                            Case "15" : Box3.Text = "FIFTEEN"
+                            Case "16" : Box3.Text = "SIXTEEN"
+                            Case "17" : Box3.Text = "SEVENTEEN"
+                            Case "18" : Box3.Text = "EIGHTEEN"
+                            Case "19" : Box3.Text = "NINETEEN"
+
+                            Case "20" : Box3.Text = "TWENTY"
+                            Case "30" : Box3.Text = "THIRTY"
+                            Case "40" : Box3.Text = "FORTY"
+                            Case "50" : Box3.Text = "FIFTY"
+                            Case "60" : Box3.Text = "SIXTY"
+                            Case "70" : Box3.Text = "SEVENTY"
+                            Case "80" : Box3.Text = "EIGHTY"
+                            Case "90" : Box3.Text = "NINETY"
+                        End Select
+
+                    Else Box3.Text = "Working..."
+
+                        Dim m, n, f1, f2 As Integer
+                        m = 10
+                        n = Int(Box2.Text)
+                        f1 = Int(n / m)
+                        f2 = n - (m * f1)
+
+                        Dim p1, p2 As String
+                        p1 = ""
+                        p2 = ""
+
+                        Select Case Int(f1)
+                            Case "2" : p1 = "TWENTY"
+                            Case "3" : p1 = "THIRTY"
+                            Case "4" : p1 = "FORTY"
+                            Case "5" : p1 = "FIFTY"
+                            Case "6" : p1 = "SIXTY"
+                            Case "7" : p1 = "SEVENTY"
+                            Case "8" : p1 = "EIGHTY"
+                            Case "9" : p1 = "NINETY"
+                        End Select
+
+                        Select Case Int(f2)
+                            Case "1" : p2 = " ONE"
+                            Case "2" : p2 = " TWO"
+                            Case "3" : p2 = " THREE"
+                            Case "4" : p2 = " FOUR"
+                            Case "5" : p2 = " FIVE"
+                            Case "6" : p2 = " SIX"
+                            Case "7" : p2 = " SEVEN"
+                            Case "8" : p2 = " EIGHT"
+                            Case "9" : p2 = " NINE"
+                        End Select
+
+                        Box3.Text = p1 + p2
+
+                    End If
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+            Box3.Text = "Only INT Number"
+
+        End Try
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://www.google.co.in/search?q=anand+vijay+rajsri")
+    End Sub
+End Class
